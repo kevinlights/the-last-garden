a90ed731e88f9feb2fc00b2612375ff9eeac23e0
Louka Soret <louka.soret@gmail.com>
2019-05-17 23:35:53 +0200

    selection

--- Mouse.gd
+++ Mouse.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = tileMap.world_to_map(get_global_mouse_position())
+	position = tileMap.map_to_world(tileMap.world_to_map(get_global_mouse_position()))
