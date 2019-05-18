c26ce389f802ecdb1cee47263bac0a90fe2990da
Louka Soret <louka.soret@gmail.com>
2019-05-18 12:18:20 +0200

    Cycle de vie pissenlit

--- Characters/Plants/Pissenlit.gd
+++ Characters/Plants/Pissenlit.gd
@@ -1,12 +1,31 @@
 extends Character
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var turns_to_hatch : int = 2
+var turns_to_fade : int = 4
 
-# Called when the node enters the scene tree for the first time.
+var sprites :Dictionary = {"icon":0,"seed":1,"adult_idle":2,"adult_wink":3}
+var current_turn : int = 0
+
 func _ready():
-	pass # Replace with function body.
+	frame = sprites["seed"]
+
+func process(delta):
+	pass
 
 func update():
+	# Timer pour le yield (pas beau)
+	yield(get_tree().create_timer(0.2), "timeout")
+	current_turn += 1
+	if current_turn == turns_to_hatch:
+		hatch()
+	if current_turn == turns_to_fade:
+		fade()
 	emit_signal("updated")
+
+# La plante eclos
+func hatch():
+	frame = sprites["adult_idle"]
+
+# La plante disparais
+func fade():
+	get_parent().remove_child(self)
