8dacbfe3481e2a3517c7d9944c2171e15dc6b324
Louka Soret <louka.soret@gmail.com>
2019-05-18 18:58:01 +0200

    resulution du bug de cannaard

--- Game.gd
+++ Game.gd
@@ -27,6 +27,7 @@ func _ready():
 	var enemy_spawn_postions : Array = tileMap.tile_peripheriques()
 	for i in range(2):
 		var test_spawn = rand_range(0,enemy_spawn_postions.size())
+		print(enemy_spawn_postions[test_spawn])
 		ajouter_instect("insectotueur", enemy_spawn_postions[test_spawn],queen_position)
 	
 func play():
@@ -71,7 +72,7 @@ func ajouter_character(character_name : String, tile : Vector2):
 	match character_name:
 		"pissenlit":
 			new_character = pissenlit_ressource.instance()
-			new_character.position += tileMap.map_to_world(tile) + Vector2(0,tileMap.cell_size.y/2)
+			new_character.position = tileMap.map_to_world(tile) + Vector2(0,tileMap.cell_size.y/2)
 	turnQueue.add_character(new_character)
 	
 func ajouter_instect(instect_name : String, tile : Vector2, cible : Vector2):
