cd968fe36caafc0cefb5f4bf844f6815b5f471e4
Guillaume Cailhe <g.cailhe@gmail.com>
2019-05-18 20:07:30 +0200

    Longévité des insectes

--- Characters/Plants/Pissenlit.gd
+++ Characters/Plants/Pissenlit.gd
@@ -5,12 +5,14 @@ var turns_to_fade : int = 4
 
 var sprites :Dictionary = {"icon":0,"seed":1,"adult_idle":2,"adult_wink":3}
 var current_turn : int = 0
+onready var longevite = get_node("Sprite2/Longevite")
 
 func _ready():
 	is_insect = false
 	$Sprite2.frame = sprites["seed"]
 	$Sprite2.material = $Sprite2.material.duplicate();
-
+	longevite.set("custom_colors/font_color", Color(0,0,0.8))
+	longevite.set_text(str(turns_to_hatch - current_turn))
 func process(delta):
 	pass
 
@@ -24,13 +26,10 @@ func update():
 		fade()
 		
 	#Update le label de longévité
-	var longevite = get_node("Sprite2/Longevite")
 	if current_turn < turns_to_hatch:
 		longevite.set_text(str(turns_to_hatch - current_turn))
-		longevite.set("custom_colors/font_color", Color(0,0,0.8))
 	else:
 		longevite.set_text(str(turns_to_fade - current_turn))
-		longevite.set("custom_colors/font_color", Color(0.8,0,0))
 	emit_signal("updated")
 
 # La plante eclos
@@ -38,6 +37,7 @@ func hatch():
 	$AnimationPlayer.play("metamorphose")
 	yield($AnimationPlayer, "animation_finished")
 	$AnimationPlayer.play("idle")
+	longevite.set("custom_colors/font_color", Color(0.8,0,0))
 	#frame = sprites["adult_idle"]
 
 # La plante disparais
